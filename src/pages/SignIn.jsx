5cabb819ccbbe98c78ffad56a56b2bdf70367f3b
Skanda-Nagaraja <s632003130@tamu.edu>
2023-12-29 15:00:20 -0600

    HomeScreen Change

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,29 +1,53 @@
-"use client"
-
 import { useState } from 'react';
-
-
-
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const SignIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+    const navigate = useNavigate(); 
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setError(null);
+
+        try {
+            // Send request to backend to check if email exists
+            const response = await axios.post('/api/check-email', { email });
+            if (response.data.exists) {
+                setError('Account with this email already exists.');
+            } else {
+                // Proceed with account creation
+            }
+        } catch (error) {
+            setError('An error occurred while checking the account.');
+        }
+    };
+
+    const handleCreateAccountClick = () => {
+        navigate('/createAccount'); 
+    };
+
+    const handleSignInClick = () => {
+        navigate('/Landing');
+        
+    }
 
     return (
         <div className='Main'>
-        <div className='mainContainer'>
-        <div className='card'>
-            <h2 className='title'>Create An Account</h2>
-            <p className='description'>Create an account and start working out today!</p>
-            <form className='signUp'>
-            <input className='input' type='email' placeholder='Email Address' />
-            <input className='input' type='password' placeholder='Password' />
-            <button className='button' type='submit'>Create Account</button>
-            </form>
-            <div className='signIn'>
-            Already Have An Account! <a href="/signin" className='link'>Sign In</a>
+            <div className='mainContainer'>
+                <div className='card'>
+                    <p className='description'>Welcome Back!</p>
+                    <form className='signUp' onSubmit={handleSubmit}>
+                        <input className='input' type='email' placeholder='Email Address' value={email} onChange={e => setEmail(e.target.value)} />
+                        <input className='input' type='password' placeholder='Password' value={password} onChange={e => setPassword(e.target.value)} />
+                        <button className='button' onClick={handleSignInClick}>Sign In</button>
+                    </form>
+                    <p className='description'>Don't have an account? Sign up now!</p>
+                    <button className='button' onClick={handleCreateAccountClick}>Create Account</button>
+                </div>
             </div>
         </div>
-        </div>
-    </div>
-    )
-    
-}
+    );
+};
