68435980d8e191fe334f937589b61a59a165203a
Skanda-Nagaraja <s632003130@tamu.edu>
2024-01-07 01:34:45 -0600

    Added Workout landing
    Need to fix some of the schema and discuss

--- src/Routes.js
+++ src/Routes.js
@@ -3,26 +3,18 @@ import { Tracking } from "./pages/Tracking";
 import { SignIn } from "./pages/SignIn";
 import { Landing } from "./pages/Landing";
 import CreateAccount from "./pages/CreateAccount";
-
-
+import CompletedWorkouts from "./Components/Workout/CompletedWorkouts.jsx"; // Corrected import
 
 export const MyRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/login" element={ <SignIn />}>
-
-                </Route>
-                <Route path="/tracking" element={ <Tracking/> }>
-                   
-                </Route>
-                <Route path="/" element={ <Landing />}>
-
-                </Route>
+                <Route path="/login" element={<SignIn />} />
+                <Route path="/tracking" element={<Tracking />} />
+                <Route path="/" element={<Landing />} />
                 <Route path="/create-account" element={<CreateAccount />} />
-
-        
+                <Route path="/completed-workouts" element={<CompletedWorkouts />} />
             </Routes>
         </Router>
-    )
+    );
 }
