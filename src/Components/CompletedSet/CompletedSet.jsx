b348c3a9b51fa95d550ff774adb9e0603152cb0d
Dyllenf <dyllenfau@gmail.com>
2024-01-09 22:47:41 -0500

    sign in api works and current workouts and exercises

--- src/Components/CompletedSet/CompletedSet.jsx
+++ src/Components/CompletedSet/CompletedSet.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
-export default function Set({ reps, weight, setNumber, onUpdate, onDelete }) {
+export default function Set({ reps, weight, setNumber, onUpdate, onDelete, completed }) {
 
+    // const [completed, setCompleted] = useState(false);
     const [editMode, setEditMode] = useState(false);
     const [currentReps, setCurrentReps] = useState(reps);
     const [currentWeight, setCurrentWeight] = useState(weight)
@@ -33,44 +34,56 @@ export default function Set({ reps, weight, setNumber, onUpdate, onDelete }) {
     return (
         <div className="p-2 mt-2 flex justify-evenly w-full font-bold text-center">
 
-            <div className="headers w-1/6">{setNumber}</div>
-            <div className="headers w-1/3"></div>
+
+            {completed ? (
+                <>
+                    <div className="headers w-1/6">{setNumber}</div>
+                    <div className="headers w-1/3"></div>
+                    <div className='headers w-1/6'>{currentReps}</div>
+                    <div className='headers w-1/6'>{currentWeight}</div>
+                </>
+                
+                
+            ) : 
+            (
+                <>
+                <div className="headers w-1/6">{setNumber}</div>
+                <div className="headers w-1/3"></div>
             
-            {editMode ? (
-            <>
-            <input
-             className="headers w-1/6 text-center"
-             type="number"
-             placeholder="Reps"
-             value={currentReps}
-             onChange={(e) => handleInputChange(e, 0)}
-             
-           />
-           <input
-             className='headers w-1/6 text-center'
-             type="number"
-             placeholder="Weight"
-             value={currentWeight}
-             onChange={(e) => handleInputChange(e, 1)}
-           />
-           <button type="submit" 
-           className="bg-cyan-400 rounded-md p-1"
-           onClick={handleSubmit}>Enter</button>
-           <button type="submit" 
-           className="bg-rose-400 rounded-md p-1"
-           onClick={handleDelete}>Delete</button>
-           </>
-           
-            ) : (
-            <>
-            <div className='headers w-1/6'>{currentReps}</div>
-            <div className='headers w-1/6'>{currentWeight}</div>
-            <button className="bg-cyan-400 rounded-md p-1" onClick={handleModeChange}> Edit </button>
-            </>
-            )
-             
-        }
-            
+                    {editMode ? (
+                    <>
+                    <input
+                    className="headers w-1/6 text-center"
+                    type="number"
+                    placeholder="Reps"
+                    value={currentReps}
+                    onChange={(e) => handleInputChange(e, 0)}
+                    />
+                    <input
+                        className='headers w-1/6 text-center'
+                        type="number"
+                        placeholder="Weight"
+                        value={currentWeight}
+                        onChange={(e) => handleInputChange(e, 1)}
+                    />
+                    <button type="submit" 
+                    className="bg-cyan-400 rounded-md p-1"
+                    onClick={handleSubmit}>Enter</button>
+                    <button type="submit" 
+                    className="bg-rose-400 rounded-md p-1"
+                    onClick={handleDelete}>Delete</button>
+                    </>
+                
+                    ) : (
+                    <>
+                    <div className='headers w-1/6'>{currentReps}</div>
+                    <div className='headers w-1/6'>{currentWeight}</div>
+                    <button className="bg-cyan-400 rounded-md p-1" onClick={handleModeChange}> Edit </button>
+                    </>
+                    
+                    )
+                     }
+            </>)}
         </div>
     );
 }
